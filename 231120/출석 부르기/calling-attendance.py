9b1a279b1fbb31a90f6068c9bf6340e0e8994752
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2023-11-20 16:49:45 +0900

    Title: 출석 부르기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231120/출석 부르기/calling-attendance.py
@@ -0,0 +1,12 @@
+sNumber = int(input())
+name = ''
+if sNumber == 1:
+    name ='John'
+elif sNumber ==2:
+    name = 'Tom'
+elif sNumber == 3:
+    name = 'Paul'
+else:
+    name = 'Vacancy'
+
+print(name)
