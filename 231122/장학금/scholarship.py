b459f886f6203de4c23eb6ce71bf7bf80d563298
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2023-11-22 16:31:38 +0900

    Title: 장학금, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231122/장학금/scholarship.py
@@ -0,0 +1,11 @@
+mid, fin = map(int, input().split())
+money = 0
+if mid >= 90:
+    if fin >= 95:
+        money = 100000
+    elif fin >= 90:
+        money = 50000
+    else:
+        money = 0
+
+print(money)
