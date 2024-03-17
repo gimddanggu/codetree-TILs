313f10a83cc0b0e682027303a24d23f07a54e2cc
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-03-18 01:06:24 +0900

    Title: 배열 채우기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/배열 채우기/filling-array.py
+++ 240318/배열 채우기/filling-array.py
@@ -1,6 +1,12 @@
 arr = list(map(int, input().split()))
 
-for i in reversed(arr):
+#for i in range(len(arr)):
+#    print(arr[len(arr)-i-1], end=' ')
+cnt = 0
+for i in arr:
+    cnt += 1
     if i == 0:
-        continue;
+        break;
+
+for i in reversed(arr[:cnt-1]):
     print(i, end=' ')
