e515e1c2eeaca47ad517d66d8adace8115c65558
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-03-24 22:06:02 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240324/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240324/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -4,7 +4,12 @@ sum1 = 0
 
 for i in range(len(arr)):
     if arr[i] == 0:
+        print(arr[i])
+        print(arr[i-1])
+        print(arr[i-2])
+        print(arr[i-3])
         sum1 = sum(arr[i-1::-1])
+
         break;
 
 
