3662b77087d4b9c2ef7e1e4a9fafbaaf5471bcb4
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-03-28 12:38:30 +0900

    Title: 숫자들의 배수, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/숫자들의 배수/multiple-of-numbers.py
+++ 240328/숫자들의 배수/multiple-of-numbers.py
@@ -1,6 +1,10 @@
 num = int(input())
+arr = []
+
 result = num
 cnt = 0
+
+"""
 while(True):
     if result % 5 == 0:
         cnt += 1
@@ -8,3 +12,21 @@ while(True):
     result += num
     if cnt == 2:
         break;
+
+if result % num == 0:
+    arr.append(result)
+if result == num * 10:
+    break;
+"""
+
+while(True):
+    arr.append(result)
+    if result % 5 == 0:
+        cnt += 1
+    result += num
+
+    if cnt == 2:
+        break;
+    
+for n in range(len(arr)):
+    print(arr[n], end=' ')
