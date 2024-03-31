1c90d0680c2d6627df12f575bba9f8bb251a3d97
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-03-31 15:09:01 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240331/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -0,0 +1,22 @@
+n = int(input())
+n_list = list(map(int, input().split()))
+
+cnt_list = [0] * 1000
+idx_list = []
+result = -1
+# 각 원소의 개수를 구한다
+# 원소의 개수가 1인 것 중 최대값 고른다.
+
+for elem in n_list:
+    cnt_list[elem] += 1
+
+for idx, c in enumerate(cnt_list):
+    if c == 1:
+        idx_list.append(idx)
+
+if not idx_list:
+    result = -1
+else:
+    result = max(idx_list)
+
+print(result)
