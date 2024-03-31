40d74e43e5e408ef62d8f6257d19cafcb9a7e01a
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-01 01:50:53 +0900

    Title: 배열의 평균, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240401/배열의 평균/ave-of-array.py
@@ -0,0 +1,16 @@
+arr = [list(map(int, input().split())) for _ in range(2)]
+allsum = 0
+# 가로 평균
+for i in range(len(arr)):
+    sumN = 0
+    for j in range(len(arr[i])):
+        sumN += int(arr[i][j])
+    print(sumN / 4, end=' ')
+print()
+
+for j in range(4):
+    verticalSum = arr[0][j] + arr[1][j]
+    print( verticalSum / 2, end=' ')
+    allsum += verticalSum
+print()
+print(allsum / 8)
