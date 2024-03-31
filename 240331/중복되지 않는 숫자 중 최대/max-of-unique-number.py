487474873883aaaf71688983a0524712367ec3d1
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-03-31 15:38:39 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240331/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240331/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -2,7 +2,8 @@ n = int(input())
 n_list = list(map(int, input().split()))
 #maxNum = max(n_list)
 #cnt_list = [0] * (maxNum+1)
-cnt_list = [0] * 1001
+maxNum = 1000
+cnt_list = [0] * (maxNum+1)
 idx_list = []
 
 # 각 원소의 개수를 구한다
@@ -11,17 +12,28 @@ idx_list = []
 for elem in n_list:
     cnt_list[elem] += 1
 
+'''
 for idx, c in enumerate(cnt_list):
     if c == 1:
         idx_list.append(idx)
+'''
 
+for idx in range(maxNum, -1, -1):
+    if cnt_list[idx] == 1:
+        result = idx
+        break;
+
+# 해설 참고해서 고침
+
+print(result)
+'''
 if not idx_list:
     result = -1
 else:
     result = max(idx_list)
 
 print(result)
-
+'''
 # 개수 배열생성
 # 1개인 개수 카운팅
 # 개수 배열 비어있으면 -1 else max값 출력
