02848699dc9c879870e7b978631f20c03e8f9a40
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-01 03:06:16 +0900

    Title: 숫자 직사각형, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240401/숫자 직사각형/rectangle-of-numbers.py
+++ 240401/숫자 직사각형/rectangle-of-numbers.py
@@ -1,6 +1,6 @@
 n, m = tuple(map(int, input().split()))
 
-arr_2d = [[i+(n-1)*j for i in range(1, m+1)] for j in range(n)]
+arr_2d = [[i+m*j for i in range(1, m+1)] for j in range(n)]
 
 for row in arr_2d:
     for elem in row:
