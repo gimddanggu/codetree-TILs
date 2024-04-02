a078aa0706110ea1675bf3c45439a685bb4cc970
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-02 21:05:35 +0900

    Title: 격자에 점 그리기, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240402/격자에 점 그리기/draw-points-on-grid.py
+++ 240402/격자에 점 그리기/draw-points-on-grid.py
@@ -1,12 +1,12 @@
 n, m = tuple(map(int, input().split()))
 
 # n*n 0으로 초기화 된 배열 생성
-arr_2d = [[0]*n for _ in range(10)] 
+arr_2d = [[0]*10 for _ in range(10)] 
 
 for i in range(1, m+1):
     a, b = tuple(map(int, input().split()))
     arr_2d[a][b] = i
-    
+
 for row in arr_2d:
     for col in row:
         print(col, end = ' ')
