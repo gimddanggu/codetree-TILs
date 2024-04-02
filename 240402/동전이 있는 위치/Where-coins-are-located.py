8c0797185c4979577fc1a8d78a459b28921f1971
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-02 21:40:28 +0900

    Title: 동전이 있는 위치, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240402/동전이 있는 위치/Where-coins-are-located.py
+++ 240402/동전이 있는 위치/Where-coins-are-located.py
@@ -4,8 +4,14 @@ arr_2d = [[0] * 10 for _ in range(10)]
 for i in range(n):
     a, b = tuple(map(int, input().split()))
     arr_2d[a][b] = 1
-
+'''
 for i in range(1, m+1):
     for j in range(1, m+1):
         print(arr_2d[i][j], end = ' ')
+    print()
+'''
+
+for row in arr_2d[1:m+1]:
+    for col in row[1:m+1]:
+        print(col, end = ' ')
     print()
