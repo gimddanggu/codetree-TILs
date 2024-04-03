60d82f5f05269b4335f827e9f5b10f0d7df8e467
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-03 11:23:22 +0900

    Title: 더 긴 문자열, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/더 긴 문자열/longer-string.py
+++ 240403/더 긴 문자열/longer-string.py
@@ -1,4 +1,13 @@
 str1, str2 = input().split()
 
-result = str1 if len(str1) > len(str2) else 'same' if len(str1) == len(str2) else str2
-print(result, len(result))
+#result = str1 if len(str1) > len(str2) else 'same' if len(str1) == len(str2) else str2
+#print(result, len(result))
+
+if len(str1) == len(str2):
+    print('same')
+
+elif len(str1) > len(str2):
+    print(str1, len(str1))
+
+else:
+    print(str2, len(str2))
