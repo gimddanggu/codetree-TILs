fc2407fb59fba16de44698f876e129dcc557b3f5
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-03 11:20:20 +0900

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/더 긴 문자열/longer-string.py
+++ 240403/더 긴 문자열/longer-string.py
@@ -1,4 +1,4 @@
 str1, str2 = input().split()
 
-result = str1 if len(str1) > len(str2) else same if str1 == str2 else str2
+result = str1 if len(str1) > len(str2) else same if len(str1) == len(str2) else str2
 print(result, len(result))
