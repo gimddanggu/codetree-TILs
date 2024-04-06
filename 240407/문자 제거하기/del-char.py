4b7fec797e799aef49e33d87d06162e866ffcc67
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-07 01:48:13 +0900

    Title: 문자 제거하기, Time: 194ms, Memory: 24MB, Status: Passed - CodeTree

--- 240407/문자 제거하기/del-char.py
+++ 240407/문자 제거하기/del-char.py
@@ -1,13 +1,16 @@
 strr = input()
+# print(strr[:-1])
 
+#for i in range(3):
 while(True):
     n = int(input())
-    strr = strr[:n] + strr[n+1:]
-    
-    if n > len(strr):
+    if n >= len(strr):
         strr = strr[:-1]
 
+    else: 
+        strr = strr[:n] + strr[n+1:]
+
     print(strr)
-    
+
     if len(strr) == 1:
         break
