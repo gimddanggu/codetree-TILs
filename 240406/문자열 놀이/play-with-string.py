87083897b3a51ca7b9870a7878af465b72a899c7
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-06 23:04:09 +0900

    Title: 문자열 놀이, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240406/문자열 놀이/play-with-string.py
+++ 240406/문자열 놀이/play-with-string.py
@@ -2,17 +2,17 @@
 # 질의 2 문자 a를 전부 문자 b로 변경한 뒤 출력
 
 strr, n = tuple(input().split())
-strr_a = ''
+strr_a = strr
 for i in range(int(n)):
     q, n1, n2 = tuple(input().split())
 
     if q == '1':
         n1 = int(n1)-1
         n2 = int(n2)-1
-        a = strr[n1]
-        b = strr[n2]
+        a = strr_a[n1]
+        b = strr_a[n2]
         for i in range(len(strr)):    
-            strr_a = strr[:n1] + b + strr[n1+1: n2] + a + strr[n2+1:]
+            strr_a = strr_a[:n1] + b + strr_a[n1+1: n2] + a + strr_a[n2+1:]
         print(strr_a) 
     else:
         '''
@@ -22,5 +22,5 @@ for i in range(int(n)):
                 print(strr_a)
         print(strr_a, 2)
         '''
-        strr_a = strr.replace(n1, n2)
+        strr_a = strr_a.replace(n1, n2)
         print(strr_a)
