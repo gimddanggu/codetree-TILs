ab09b6ec66fbc304f90db327ddea02660ec098a0
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-10 12:56:50 +0900

    Title: 미는 횟수, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/미는 횟수/number-of-pushes.py
+++ 240410/미는 횟수/number-of-pushes.py
@@ -2,10 +2,10 @@ str1 = input()
 str2 = input()
 
 for i in range(len(str1)):
-    if str1 == str2:
-        print(i)
-        break;
     str1 = str1[1:] + str1[0]
+    if str1 == str2:
+        print(i+1)
+        break;
         
 if str1 != str2:
     print(-1)
