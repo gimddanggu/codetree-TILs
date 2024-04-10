c7a8d888ffef3cc50ab64d85b05b76790073f931
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-10 14:00:02 +0900

    Title: 미는 횟수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240410/미는 횟수/number-of-pushes.py
+++ 240410/미는 횟수/number-of-pushes.py
@@ -1,11 +1,13 @@
 str1 = input()
 str2 = input()
-
+#str1 = 'abababa'
 for i in range(len(str1)):
-    str1 = str1[1:] + str1[0]
+    str1 = str1[-1] + str1[:-1]
+
+    
     if str1 == str2:
         print(i+1)
         break;
-        
+
 if str1 != str2:
     print(-1)
