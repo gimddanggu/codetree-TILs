7a8b2f995b12ae879086eb297257996e2d257f71
gimddanggu <107992381+gimddanggu@users.noreply.github.com>
2024-04-19 19:10:50 +0900

    Title: 1이 되는 순간까지, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240419/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -0,0 +1,14 @@
+def rec(n):
+    if n == 1:
+        return 0
+
+    if n % 2 == 0:
+        return rec(n // 2) + 1
+            
+    else:
+        return rec(n // 3) + 1
+        
+    
+
+
+print(rec(230))
