4167835779f0d6f2f2f07813508db07efba6696a
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-09 09:27:05 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/비교 연산/comparison-operator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    cout << (a >= b) ? 1 : 0;
+    cout << '\n';
+    cout << (a > b) ? 1 : 0;
+    cout << '\n';
+    cout << (a <= b) ? 1 : 0;
+    cout << '\n';
+    cout << (a < b) ? 1 : 0;
+    cout << '\n';
+    cout << (a == b) ? 1 : 0;
+    cout << '\n';
+    cout << (a != b) ? 1 : 0;
+
+    return 0;
+}
