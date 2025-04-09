b24c355d98067b0f91ea693cb526f08e4ee03927
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-09 09:51:03 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    cout << (a < b) ? 1 : 0;
+    cout << ' ';
+    cout << (a == b) ? 1 : 0;
+
+    return 0;
+}
