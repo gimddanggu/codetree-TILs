55f6e4d7bcd2fdc23d2d7f2976a8ff85c5506582
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-09 10:07:29 +0900

    Title: 등급 매기기, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/등급 매기기/ranking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    char grade;
+
+    cin >> a;
+    if (a >= 90) grade = 'A';
+    else if (a >= 80) grade = 'B';
+    else if (a >= 70) grade = 'C';
+    else if (a >= 60) grade = 'D';
+    else grade = 'F';
+    
+    cout << grade;
+    return 0;
+}
