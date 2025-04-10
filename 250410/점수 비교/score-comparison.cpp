9fcc00a37c92de0b895697977c10db263bf8c7a4
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-10 23:45:13 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250410/점수 비교/score-comparison.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+
+    int c, d;
+    cin >> c >> d;
+
+    if (a > c && b > d) cout << 1;
+    else cout << 0;
+    return 0;
+}
