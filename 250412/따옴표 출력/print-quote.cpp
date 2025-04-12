a9edd2b1332e0f3a214aed6d813060a977d013e2
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-12 19:01:07 +0900

    Title: 따옴표 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250412/따옴표 출력/print-quote.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    string st = "He says \"It's a really simple sentence\".";
+    cout << st;
+    return 0;
+}
