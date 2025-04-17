ad1a7556d08507bb73e799c6045c3d88c158fdbb
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-17 16:06:21 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250417/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+
+def sol(a, b, i):
+    if i == n:
+        return a
+    if a < b:
+        a, b = b, a
+
+    if a % b == 0:
+        return sol(a, arr[i], i+1)
+    else :
+        return sol(a*b, arr[i], i+1)
+print(sol(arr[0], arr[1], 2))
+
