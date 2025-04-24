e58e43a69580ca05b1296cdec740508e5fa74621
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-24 09:21:55 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250424/Date to Date/date-to-date.py
+++ 250424/Date to Date/date-to-date.py
@@ -2,13 +2,13 @@ m1, d1, m2, d2 = map(int, input().split())
 m1Day, m2Day = 0, 0
 month_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for i in range(m1+1):
+for i in range(m1):
     m1Day += month_day[i]
 m1Day += d1
 
-for i in range(m2+1):
+for i in range(m2):
     m2Day += month_day[i]
 m2Day += d2
 
-print(m2Day-m1Day)
+print(m2Day-m1Day+1)
 # Please write your code here.
