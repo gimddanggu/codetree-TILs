dfb4972162ec8ef01b7a9d54f66ac57db6bf2bce
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2025-04-24 11:40:43 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250424/DateTime to DateTime/datetime-to-datetime.py
+++ 250424/DateTime to DateTime/datetime-to-datetime.py
@@ -2,7 +2,11 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 def calcDayTime(day, hour, minute):
-    if (day <= 11 and hour <= 11 and minute <= 11):
+    if (day < 11 ):
+        return -1
+    elif (day == 11 and hour < 11) :
+        return -1
+    elif (day == 11 and hour == 11 and minute < 11):
         return -1
     
     dayTmin = day * 24 * 60
@@ -12,5 +16,7 @@ def calcDayTime(day, hour, minute):
 
 # print(calcDayTime(11, 11, 11))
 # print(calcDayTime(a, b, c))
-
-print(calcDayTime(a, b, c) - calcDayTime(11, 11, 11))
+if (1 if calcDayTime(a, b, c) != -1 else 0):
+    print(calcDayTime(a, b, c) - calcDayTime(11, 11, 11))
+else:
+    print(-1)
